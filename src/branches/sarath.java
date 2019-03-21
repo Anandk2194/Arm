27124020e6dd746f2f65a70f917531df3e0fdfb7
visalvani <vs.visalakshi@gmail.com>
2019-03-21 08:39:33 +0530

    added

---
+++ src/branches/sarath.java
@@ -0,0 +1,44 @@
+package branches;
+
+public class GreensTechnology {
+
+	public void omrBranch() {
+		System.out.println("omrBranch");
+
+
+}
+	public void adayarBranch() {
+	System.out.println("adayarBranch");
+
+	}
+	public void siruseri() {
+		System.out.println("siruseriBranch");
+		
+
+	}
+	public void tambaram() {
+		System.out.println("tambaramBranch");
+	
+
+	}
+	public void annanagar() {
+		System.out.println("annanagar");
+
+	}
+	public void velachery() {
+		System.out.println("velachery");
+		
+
+	}
+	public static void main(String[] args) {
+		GreensTechnology object1= new GreensTechnology();
+				object1.omrBranch();
+		object1.adayarBranch();
+		object1.siruseri();
+		object1.tambaram();
+		object1.annanagar();
+		object1.velachery();
+	}
+	
+}
+
